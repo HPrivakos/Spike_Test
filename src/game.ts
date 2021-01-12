ed5692ab0811467206d34641890a01dfd810c36f
Syed <syedibrahim1388@gmail.com>
2021-01-11 22:51:25 -0500

    comment edit

--- src/game.ts
+++ src/game.ts
@@ -23,7 +23,12 @@ coin.addComponent(new Transform({
 
 coin.addComponent(new GLTFShape("models/pirate coin finall v2.gltf"))
 
+// Check to see if user has this nft in their wallet: 
+// https://opensea.io/assets/0xe39a238d74bdd95a895026fc25ec97fb8a4b1959/37
+
 // If user has the following nft in their wallet console.log (Approved)
 // Or else console.log (not approved)
+
+// Please include comments for each section of code so it will be easier for me to understand whats going on
 
 // It would be nice if the coin could rotate if the user had the NFT...but not required
